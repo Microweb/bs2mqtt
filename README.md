e5d3db6b2fd21b301d86c6f6707634a27569850b
Piotr Smialkowski <p.smialkowski@gmail.com>
2022-10-14 20:34:03 +0200

    Update info readme

--- README.md
+++ README.md
@@ -1,10 +1,51 @@
 # BeSecure3 to MQTT
 
+MQTT wrapper around the python BiSecur Gateway 
+It's based on library developed by [pysecur3](https://github.com/skelsec/pysecur3) @skelsec for Hormann Bisecure Gateway devices.
+
 ## Instalation
 
 ```bash
-git clone https://github.com/Microweb/bs2mqtt.git
-cd bs2mqtt
-python3 -m bs2mqtt scan # find available gateway device in your local networl
+sudo mkdir /opt/bs2mqtt
+sudo chown -R ${USER}: /opt/bs2mqtt
 
+git clone https://github.com/Microweb/bs2mqtt.git /opt/bs2mqtt
+
+cd /opt/bs2mqtt
+./update.sh
+bs2 scan # find available gateway device in your local networl
+```
+
+## Configuring
+
+Before you start bs2mqtt (BeSecure3 to MQTT) you need to create file `configuration.json`
+
+
+Open configuration file and write:
+
+```bash
+vi /opt/bs2mqtt/configuration.json
+```
+
+Basic configuration can looks like that:
+
+```json
+{
+    "mqtt": {
+        "base_topic": "bs3mqtt",
+        "server": "mqtt://localhost",
+        "user": "",
+        "password": ""
+    },
+    "devices": {
+        "1": "door1",
+        "2": "gate1"
+    }
+}
+```
+
+## Starting bs2mqtt
+
+```bash
+bs3 serve --config /opt/bs2mqtt/configuration.json
 ```
--- bs2mqtt/__main__.py
+++ bs2mqtt/__main__.py
@@ -1,4 +1,7 @@
 import argparse
+from email.policy import default
+import os
+import pathlib
 
 from .commands import ScanCommand, ServeCommand, InitCommand
 
@@ -10,24 +13,29 @@ COMMANDS = {
 }
 
 def main():
-    root = argparse.ArgumentParser(prog='PROG')
+    cwd = pathlib.Path.cwd()
+    default_file = str(cwd / "configuration.json")
 
-    sub = root.add_subparsers(dest='command')
-    scan_parser = sub.add_parser('scan')
-    scan_parser.add_argument('-i', '--listen-ip', default='', help='IP to listen incoming packets on. Default: all available ips')
-    scan_parser.add_argument('-b', '--broadcast-ip', default='255.255.255.255', help='Broadcast address. Default: 255.255.255.255')
+    root = argparse.ArgumentParser(prog="PROG")
 
-    init_parser = sub.add_parser('init')
-    init_parser.add_argument('-c', '--config', default='', help='IP to listen incoming packets on. Default: all available ips')
+    sub = root.add_subparsers(dest="command")
+    scan_parser = sub.add_parser("scan")
+    scan_parser.add_argument("-i", "--listen-ip", default="", help="IP to listen incoming packets on. Default: all available ips")
+    scan_parser.add_argument("-b", "--broadcast-ip", default="255.255.255.255", help="Broadcast address. Default: 255.255.255.255")
 
-    serve_parser = sub.add_parser('serve')
-    serve_parser.add_argument('-c', '--config', default='', help='IP to listen incoming packets on. Default: all available ips')
+    init_parser = sub.add_parser("init")
+    init_parser.add_argument("--config", default=default_file, help="Path to configuration file")
+
+    serve_parser = sub.add_parser("serve")
+    serve_parser.add_argument("--config", default=default_file, help="Path to configuration file")
 
     args = root.parse_args()
-    COMMANDS[args.command].execute(args)
+    try:
+        COMMANDS[args.command].execute(args)
+    except KeyError:
+        print(f"Missing command")
 
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
